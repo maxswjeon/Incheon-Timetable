9c8090d4cee080ea2c00fafe882b00de3da71bef
Jeon Sang Wan <maxswjeon@naver.com>
2018-05-22 01:41:12 +0900

    Modulize Init Files
    Modulized Express Init File
    Added Comments And Type Info (JSdoc)

--- src/bin/express.js
+++ src/bin/express.js
@@ -3,6 +3,7 @@ const path = require('path');
 const fs = require('fs');
 
 //Logger Modules
+const winston = require('winston');
 const morgan = require('morgan');
 
 //Express JS modules
@@ -16,12 +17,45 @@ const ejs = require('ejs');
 //Express JS Configureation
 const app = express();
 
+/**
+ * Set View Engine for Express Server.  
+ * (default : EJS)
+ */
 function SetRenderer() {
 	//Renderer (EJS)
 	app.set('view engine', 'ejs');
 	app.engine('html', ejs.renderFile);
 }
 
+/**
+ * Set Root Path to View Files for Renderer.  
+ * 
+ * @param {String} viewPath Root Path to view Files
+ */
+function SetRenderFolder(viewPath) {
+	if (!path.isAbsolute(viewPath)) {
+		viewPath = path.join(global.rootPath, viewPath);
+	}
+	app.set('views', viewPath);
+}
+
+/**
+ * Set Root Path to Asset Files for Server.  
+ * Asset Folder will be sended as-is.  
+ * Client-side JS Files and CSS Files will be recommended.  
+ * 
+ * @param {String} assetPath Root Path to assets
+ */
+function SetAssetFolder(assetPath) {
+	if (!path.isAbsolute(assetPath)) {
+		assetPath = path.join(global.rootPath, assetPath);
+	}
+	app.use(express.static(assetPath));
+}
+
+/**
+ * Set Security Modules and Configure Major Security Hazards
+ */
 function SetSecurityModule() {
 	//Security
 	app.use(helmet({
@@ -31,11 +65,19 @@ function SetSecurityModule() {
 	app.disable('x-powered-by');
 }
 
-function SetSessionModule() {
+/**
+ * Set Session Module and Configure It
+ * 
+ * @param {String} _secret Secret String For Encrypting Sessions.  
+ * Default is Configured in config.secret.json 
+ */
+function SetSessionModule(_secret) {
+	const secret = _secret ||  global.secret.session.secret;
+
 	//Session
 	app.set('trust proxy', 1);
 	app.use(session({
-		secret : global.secret.session.secret,
+		secret : secret,
 		name : 'INCHEONSESSID',
 		cookie : {
 			path : '/',
@@ -48,6 +90,12 @@ function SetSessionModule() {
 	}));
 }
 
+/**
+ * Set Parser Modules and Configure It
+ * 
+ * @param {String} _secret Secret String For Encrypting Sessions.  
+ * Default is Configured in config.secret.json 
+ */
 function SetParser() {
 	//Logger And Parsers
 	app.use(bodyParser.json());
@@ -55,6 +103,10 @@ function SetParser() {
 	app.use(cookieParser());
 }
 
+/**
+ * Set Logger Modules and Configure It  
+ * Set access.log and console log
+ */
 function SetLogger() {
 	app.use(morgan('dev'));
 	app.use(morgan('combined', {
@@ -62,31 +114,12 @@ function SetLogger() {
 	}));
 }
 
-function ImportRoutes() {
-	//Express Routers
-	const routes = {};
-
-	routes.index = require('../routes/index');
-	routes.about = require('../routes/about');
-	routes.security = require('../routes/security');
-
-	routes.enroll = {};
-	routes.enroll.index = require('../routes/enroll/index');
-
-	routes.view = {};
-	routes.view.index = require('../routes/view/index');
-
-	routes.board = {};
-	routes.board.index = require('../routes/board/index');
-
-	routes.user = {};
-	routes.user.login = require('../routes/user/login');
-	routes.user.signup = require('../routes/user/signup');
-
-	return routes;
-}
-
-function SetRoutes(app, routes) {
+/**
+ * Adds All Routers Recusively in `routes`
+ * 
+ * @param {String} routes JSON object includes routers
+ */
+function SetRoutes(routes) {
 	for (let key in routes) {
 		if (typeof routes[key] == 'function') {
 			app.use('/', routes[key]);
@@ -97,12 +130,57 @@ function SetRoutes(app, routes) {
 	}
 }
 
-function HandleError(req, res, next) {
+/**
+ * Adds All Routers Recusively in Directory
+ * 
+ * @param {String} routePath Root path to Start Finding Routers
+ * 
+ * @returns {Boolean} false if `routePath` is not Directory
+ */
+function SetAllRoutes(routePath) {
+	if (!path.isAbsolute(routePath)) {
+		routePath = path.join(global.rootPath, routePath);
+	}
+
+	if (!fs.statSync(routePath).isDirectory()) {
+		winston.error('ImportAllRoutes Failed : %s is not directory', routePath);
+		return false;
+	}
+
+	const files = fs.readdirSync(routePath);
+	for(const index in files) {
+		const file = files[index];
+		const filePath = path.join(routePath, file);
+		if (fs.statSync(filePath).isDirectory()) {
+			SetAllRoutes(filePath);
+		}
+		else if (/.+\.js$/.test(file)) {
+			app.use('/', require(filePath));
+		}
+	}
+}
+
+/**
+ * Set All Error Handlers in `ErrorHandler`
+ */
+function SetErrorHandlers() {
+	for (const key in this.ErrorHandler) {
+		app.use(this.ErrorHandler[key]);
+	}
+}
+
+/**
+ * Handles Not Found Error
+ */
+function Handle404(req, res, next) {
 	var err = new Error('Not Found');
 	err.status = 404;
 	next(err);
 }
 
+/**
+ * Renders Error Page
+ */
 function RenderError(err, req, res) {
 	const error = req.app.get('env') === 'development' ? err : {};
 
@@ -116,23 +194,35 @@ function RenderError(err, req, res) {
 	});
 }
 
-SetRenderer();
-SetSecurityModule();
-SetSessionModule();
-SetParser();
-SetLogger();
+/**
+ * Starts ExpressJS Server
+ * 
+ * @param {Number} _port Specifies the port to listen. 
+ * Default is defined at `global.express.port`. If not also defined,
+ * defaults to `3000`.
+ */
+function Start(_port) {
+	const port = _port || global.express.port || 3000;
+	app.listen(port, () => {
+		winston.info('Express Server has started on port ' + port);
+	});
+}
 
-app.set('views', path.join(global.rootPath, 'src/views'));
-app.use(express.static(path.join(global.rootPath, 'src/assets')));
+module.exports = {
+	setRenderer : SetRenderer,
+	setRenderFolder : SetRenderFolder,
+	setAssetFolder : SetAssetFolder,
+	setSecurityModule : SetSecurityModule,
+	setSessionMoudle : SetSessionModule,
+	setParser : SetParser,
+	setLogger : SetLogger,
+	setRoutes : SetRoutes,
+	setAllRoutes : SetAllRoutes,
+	setErrorHandlers : SetErrorHandlers,
+	ErrorHandler : {
+		404 : Handle404,
+		Render : RenderError
+	},
 
-const routes = ImportRoutes();
-SetRoutes(app, routes);
-
-// catch 404 and forward to error handler
-app.use(HandleError);
-
-// error handler
-app.use(RenderError);
-
-
-module.exports = app;
+	start : Start,
+};
--- src/index.js
+++ src/index.js
@@ -1,20 +1,30 @@
 const config = require('./bin/config');
-const app = require('./bin/express');
+const express = require('./bin/express');
 const mongodb = require('./bin/mongodb');
 const winston = require('./bin/logger');
 const eventHandler = require('./bin/eventHandler');
 
 const dataLoader = require('./bin/dataLoader');
 
-const port = global.express.port || 3000;
+//Configuring ExpressJS
+express.setRenderer();
+express.setRenderFolder('src/views');
+express.setAssetFolder('src/assets');
 
-app.listen(port, () => {
-	winston.info('Express Server has started on port ' + port);
-});
+express.setSecurityModule();
+
+express.setSessionMoudle();
+
+express.setParser();
+express.setLogger();
+
+express.setAllRoutes('src/routes');
+
+express.setErrorHandlers();
+express.start();
 
 module.exports = {
 	config : config,
-	express : app,
 	mongodb : mongodb,
 	logger : winston,
 	dataSheet : dataLoader,
